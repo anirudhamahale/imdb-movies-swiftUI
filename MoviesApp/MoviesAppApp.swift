6f10add1837fa9afa1a8d357693010487000a012
Anirudha Mahale <anirudhamahale@gmail.com>
2022-07-16 11:06:24 +0530

    Initial Commit

---
+++ MoviesApp/MoviesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoviesAppApp.swift
+//  MoviesApp
+//
+//  Created by Anirudha Mahale on 16/07/22.
+//
+
+import SwiftUI
+
+@main
+struct MoviesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
