ca5903b05a81c35928789fbc7a14b52d0985185d
Anirudha Mahale <anirudhamahale@gmail.com>
2022-07-16 11:16:40 +0530

    first commit

---
+++ MoviesApp/Views/ContentView.swift
@@ -0,0 +1,15 @@
+//
+//  ContentView.swift
+//  MoviesApp
+//
+//  Created by Anirudha Mahale on 16/07/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+  var body: some View {
+    Text("Hello, world!")
+      .padding()
+  }
+}
